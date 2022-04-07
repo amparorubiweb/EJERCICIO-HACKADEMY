eb2e6334b36c2998d632631df20d3d2b77eadb3f
amparorubiweb <101374035+amparorubiweb@users.noreply.github.com>
2022-04-07 16:40:35 -0500

    Add files via upload

---
+++ registration-form/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import RegistrationForm from './components/RegistrationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="App-content">
+        <h3 className="App-header">Registration Form</h3>
+        <RegistrationForm />
+      </section>
+    </div>
+  );
+}
+
+export default App;
